291d78f4eaa93c13698965c0df0d4a86898e113d
BestEngineer <65943291+bestpisit@users.noreply.github.com>
2024-01-25 00:11:56 +0700

    activities connection

--- main.go
+++ main.go
@@ -7,34 +7,60 @@ import (
 	"os"
 )
 
-type Activitie struct {
-	Code string
-	Desc string
-	ES int
-	Duration int
-	EF int
-	LS int
-	Slack int
-	LF int
-	Edges []Edge
-	Dependencies []Activitie
-	Dependents []string
+type Activity struct {
+	Code         string
+	Desc         string
+	ES           int
+	Duration     int
+	EF           int
+	LS           int
+	Slack        int
+	LF           int
+	Edges        []Edge
+	Dependencies []Activity
+	Dependents   []string
 }
 
 type Edge struct {
-	Src Activitie
-	Dest Activitie
+	Src  string
+	Dest string
 }
 
 func main() {
 	fmt.Println("Loading project activities")
 	jsonFile, err := os.Open("BuildingAHouse.json")
 	if err != nil {
-		fmt.Print(err)
+		fmt.Println(err)
+		return
 	}
-	fmt.Println("Project activities Loaded")
-	byteValue, _ := ioutil.ReadAll(jsonFile)
-	var activities []Activitie
-	json.Unmarshal(byteValue,&activities)
-	
+	defer jsonFile.Close()
+
+	fmt.Println("Project activities loaded")
+
+	byteValue, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	var activities []Activity
+	if err := json.Unmarshal(byteValue, &activities); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	activityNetwork := make(map[string]*Activity)
+	for i, activity := range activities {
+		activityNetwork[activity.Code] = &activities[i]
+	}
+
+	for _, activity := range activities {
+		for _, dependent := range activity.Dependents {
+			dependentActivity, exists := activityNetwork[dependent]
+			if exists {
+				dependentActivity.Dependencies = append(dependentActivity.Dependencies, activity)
+			}
+		}
+	}
+
 }
