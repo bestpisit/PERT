83df959860c6792b22253a71cf265ee1da3b83e9
bestpisit <bestpisit@hotmail.com>
2024-01-24 15:05:52 +0700

    Structure Init

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
-module (
-	"fmt"
+import (
+    "fmt"
 )
 
-func main(){
-	fmt.Print("Nice")
+type PERT struct {
+	Text string
+	ES int
+	Time int
+	EF int
+	LS int
+	Slack int
+	LF int
+	Edges []Edge
+	Dependencies []PERT
+}
+
+type Edge struct {
+	Src PERT
+	Dest PERT
+}
+
+func main() {
+	fmt.Print("Hello")
 }
