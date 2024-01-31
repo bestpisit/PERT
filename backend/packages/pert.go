5db9497444e95007980ca723c7801ab544cbe33a
BestEngineer <65943291+bestpisit@users.noreply.github.com>
2024-01-31 23:55:01 +0700

    done

--- backend/packages/pert.go
+++ backend/packages/pert.go
@@ -119,45 +119,86 @@ func PERT(activityNetwork map[string]*Activity) map[string]*activityCase {
 	startCase := &activityCase{activityNetwork["Start"], -1, activityNetwork["Start"].Duration, -1, -1, -1, -1}
 	queueCase.PushBack(startCase)
 	activityCases := make(map[string]*activityCase)
+	doneCases := make(map[string]bool)
 	for queueCase.Len() > 0 {
 		element := queueCase.Front()
 		currentCase := element.Value.(*activityCase)
 		queueCase.Remove(element)
 		existCase, exist := activityCases[currentCase.activity.Code]
-		if exist && currentCase != existCase {
-			continue
-		}
-		if !exist {
-			activityCases[currentCase.activity.Code] = currentCase
-		} else {
-			currentCase = activityCases[currentCase.activity.Code]
+		if currentCase.LS == -1 {
+			if exist && currentCase != existCase {
+				continue
+			}
+			if !exist {
+				activityCases[currentCase.activity.Code] = currentCase
+			} else {
+				currentCase = activityCases[currentCase.activity.Code]
+			}
 		}
 		//check parent
 		pass := true
-		if len(currentCase.activity.Dependents) > 0 {
-			maxEF := 0
-			for _, dependent := range currentCase.activity.Dependents {
-				parent, exist := activityCases[dependent]
-				if !exist || parent.EF == -1 {
-					pass = false
-					break
+		if currentCase.EF != -1 {
+			if currentCase.activity.Code == "End" {
+				currentCase.LF = currentCase.EF
+			} else {
+				minLS := -1
+				for _, edge := range currentCase.activity.Edges {
+					successor, exist := activityCases[edge.Dest]
+					if !exist {
+						continue
+					} else if successor.LS == -1 {
+						pass = false
+						break
+					}
+					if minLS == -1 || minLS > successor.LS {
+						minLS = successor.LS
+					}
 				}
-				if maxEF < parent.EF {
-					maxEF = parent.EF
-				}
+				currentCase.LF = minLS
 			}
-			currentCase.ES = maxEF
-		} else { // means it start
-			currentCase.ES = 0
+		} else {
+			if len(currentCase.activity.Dependents) > 0 {
+				maxEF := 0
+				for _, dependent := range currentCase.activity.Dependents {
+					parent, exist := activityCases[dependent]
+					if !exist || parent.EF == -1 {
+						pass = false
+						break
+					}
+					if maxEF < parent.EF {
+						maxEF = parent.EF
+					}
+				}
+				currentCase.ES = maxEF
+			} else { // means it start
+				currentCase.ES = 0
+			}
 		}
 		if pass { //branching
-			currentCase.EF = currentCase.ES + currentCase.Duration
-			fmt.Println(currentCase.activity.Code, currentCase.activity.Dependents, currentCase.ES, currentCase.Duration, currentCase.EF)
-			for _, edge := range currentCase.activity.Edges {
-				newCase := &activityCase{activityNetwork[edge.Dest], -1, activityNetwork[edge.Dest].Duration, -1, -1, -1, -1}
-				queueCase.PushBack(newCase)
+			if currentCase.EF == -1 {
+				currentCase.EF = currentCase.ES + currentCase.Duration
+				// fmt.Println(currentCase.activity.Code, currentCase.activity.Dependents, currentCase.ES, currentCase.Duration, currentCase.EF)
+				for _, edge := range currentCase.activity.Edges {
+					newCase := &activityCase{activityNetwork[edge.Dest], -1, activityNetwork[edge.Dest].Duration, -1, -1, -1, -1}
+					queueCase.PushBack(newCase)
+				}
+			} else {
+				currentCase.LS = currentCase.LF - currentCase.Duration
+				currentCase.Slack = currentCase.LF - currentCase.EF
+				for _, dependent := range currentCase.activity.Dependents {
+					_, exist := doneCases[dependent]
+					if !exist {
+						doneCases[dependent] = true
+						queueCase.PushBack(activityCases[dependent])
+					}
+				}
+				fmt.Println(currentCase.activity.Code, currentCase.Slack)
+				// fmt.Println(currentCase.activity.Code, currentCase.activity.Dependents, currentCase.activity.Edges, currentCase.ES, currentCase.Duration, currentCase.EF, "|", currentCase.LS, currentCase.Slack, currentCase.LF)
 			}
 		} else { //revert
+			queueCase.PushBack(currentCase)
+		}
+		if currentCase.activity.Code == "End" && currentCase.EF != -1 && currentCase.LS == -1 {
 			queueCase.PushBack(currentCase)
 		}
 	}
